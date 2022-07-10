1191764c1b62a84a84a04afc56517b3d80fa96f8
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-10 15:39:20 +0530

    Update Value.js

--- src/comps/Value.js
+++ src/comps/Value.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import Card from './Card';
 
+
+const Value = (props) => {
+ return (
+  <> {
+    (props.data.length > 0) ? props.data.map(item => {
+             return <Card imgUrl={item.imageUrl} id={item.user} />
+    }) : <p style={{ color: "white" }}>No posts Available</p>
+  }
+  </>
+  )
+}
+
+export default Value;
