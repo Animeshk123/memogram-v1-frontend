204540680695a0da29d3c8c9e4260bb04a9d2418
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-10 13:58:58 +0530

    Update Get.js

--- src/logic/Get.js
+++ src/logic/Get.js
@@ -1,8 +1,9 @@
-const getUser = async (setUser) => {
+const getUser = async (setUser,setProfile) => {
     let response = await fetch(`${process.env.REACT_APP_URL}/api/v1/user?id=${localStorage.getItem("id")}`);
     let data = await response.json();
     if (data.auth) {
         setUser(data.user);
+        setProfile(data.user.userProfile);
     }
     else {
         localStorage.removeItem("id");
