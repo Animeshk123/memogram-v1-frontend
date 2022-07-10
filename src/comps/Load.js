045adbba3df1e1976d3ef1c8f69441c30641ad9a
Animesh kumbhakar <75767221+Animeshk123@users.noreply.github.com>
2022-07-10 15:27:35 +0530

    Create Load.js

---
+++ src/comps/Load.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Load = () => {
+ return (
+   <>
+     <CardWrapper></CardWrapper>
+   </>
+  );
+}
+
+const CardWrapper = styled.div`
+  width:100%;
+  max-width:450px;
+  border-radius:80px;
+  height:200px;
+  background:'rgba(255,255,255,0.7);
+  margin-bottom:1.2rem;
+`;
+
+
+
+export default Load;
